0765c6dd3b9491695238aaef2ba37347c89ecd7e
williamhansson <williamhansson@icloud.com>
2018-11-10 14:48:58 +0800

    second time comit

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.template.loader import get_template
+from django.http import HttpResponse
+from datetime import datetime
+from models import Post
 
 # Create your views here.
+
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    post_lists = list()
+    html = template.render(locals())
+  #  for count, post in enumerate(posts):
+  #      post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
+  #      post_lists.append("<small>" + str(post.body.encode('utf-8')) + "</small><br><br>")
+
+    return HttpResponse(html)
